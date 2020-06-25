4c06700cd56ba253be94eb97bcc9562a866ef0d6
Stuart Pomeroy <rushmead@gmail.com>
2020-06-25 15:42:07 +0100

    Add some more debug messages

--- src/modules/websocket/mutations.ts
+++ src/modules/websocket/mutations.ts
@@ -33,7 +33,7 @@ export const mutations: MutationTree<SocketState> = {
             state.modal = null;
         }
         state.socket.message = message;
-        // console.log(JSON.stringify(message));
+        console.log(JSON.stringify(message));
     },
     SOCKET_RECONNECT(state: any, count: number) {
         console.info(`Attempting to reconnect to java-backend, tries: ${count}`);
