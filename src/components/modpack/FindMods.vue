2275b4d4f3a483d9e9ec6509a0219154a89ee754
Michael Hillcox <michaelhillcox@gmail.com>
2021-07-10 16:54:17 +0100

    feat: empty states

--- src/components/modpack/FindMods.vue
+++ src/components/modpack/FindMods.vue
@@ -35,7 +35,51 @@
         </div>
       </div>
       <div class="results">
-        <mod-card v-for="mod in results" :key="mod.id" :mod="mod" :instance="instance" :target="target" />
+        <template v-if="search !== '' && results.length">
+          <mod-card v-for="(mod, index) in results" :key="index" :mod="mod" :instance="instance" :target="target" />
+        </template>
+        <!-- This is so over the top -->
+        <div class="no-results" v-else-if="!results.length && search !== '' && !loadingTerm && !loadingExtra">
+          <div
+            class="fancy"
+            data-balloon-length="large"
+            aria-label="I'm a missing texture if you didn't know... Get it?"
+            data-balloon-blunt
+            data-balloon-pos="down"
+          >
+            <svg viewBox="0 0 268 298" fill="none" xmlns="http://www.w3.org/2000/svg" class="missing-box">
+              <path d="M134 134L0 67V230L134 298V134Z" class="inverse" />
+              <path d="M134 133.645L67 100V181.853L134 216V133.645Z" class="prim-light" />
+              <path d="M67 181.645L0 148V230L67 264V181.645Z" class="prim-light" />
+              <path d="M134 134L268 67V230L134 298V134Z" class="inverse" />
+              <path d="M134 215.645L201 182V264L134 298V215.645Z" class="prim-dark" />
+              <path d="M201 100.645L268 67V148.853L201 182V100.645Z" class="prim-dark" />
+              <path d="M134 134L0 67L134 0L268 67L134 134Z" class="inverse" />
+              <path d="M67 101L0 67L67 33.5L134 67.5L67 101Z" class="prim-light" />
+              <path d="M201 101L134 67.5L201 33.5L268 67L201 101Z" class="prim-light" />
+            </svg>
+          </div>
+          <div class="text">
+            <p class="title">No results found 😢</p>
+            <div>
+              Looks like nothing was found for <code>{{ search }}</code
+              >, try a different sarch.
+            </div>
+          </div>
+        </div>
+        <div class="make-a-search" v-else-if="search === ''">
+          <div class="search-prompt">
+            <div class="icon-container">
+              <font-awesome-icon icon="search" class="primary" />
+              <font-awesome-icon icon="search" class="secondary" />
+            </div>
+            <p>Use the search box above to find new mods</p>
+          </div>
+        </div>
+        <div class="loading" v-else>
+          <font-awesome-icon spin icon="spinner" />
+          <p>Loading results</p>
+        </div>
       </div>
       <div class="more-btn flex justify-center">
         <ftb-button
@@ -85,8 +129,16 @@ export default class FindMods extends Vue {
   private offset = 0;
 
   search = '';
+
+  // Handles loading the packs version data
   loading = true;
+
+  // Handles loading results
   loadingExtra = false;
+
+  // Handles loading the inital search term
+  loadingTerm = false;
+
   target: string = '';
 
   timeTaken = 0;
@@ -126,10 +178,13 @@ export default class FindMods extends Vue {
       return;
     }
 
+    this.loadingTerm = true;
     const start = new Date().getTime();
     const searchResults = await axios.get<ModSearchResults>(
       `${process.env.VUE_APP_MODPACK_API}/public/mod/search/${this.target || 'all'}/100?term=${this.search}`,
     );
+
+    this.loadingTerm = false;
 
     this.resetSearch();
     if (!searchResults || searchResults.data?.total === 0) {
@@ -145,7 +200,12 @@ export default class FindMods extends Vue {
   }
 
   /**
-   * Maintaines an offset and hard limit of the results
+   * Maintaines an offset and hard limit of the results, at the same time, after
+   * each fetch, we make another request to buffer the next 5 results. This makes
+   * the 'load more' button seem near instant for anyone not going to fast.
+   *
+   * NOTE: the buffer could logically get really big but as we're only able
+   * to handle 100 results at the max right now, this is a non-issue
    */
   private async loadResultsProgressively() {
     if (!this.hasResults || this.loadingExtra) {
@@ -218,6 +278,7 @@ export default class FindMods extends Vue {
     this.initialTimeTaken = 0;
     this.loadingExtra = false;
     this.resultsBuffer = [];
+    this.loadingTerm = false;
   }
 
   /**
@@ -289,6 +350,139 @@ export default class FindMods extends Vue {
 
     .value {
       font-family: Arial, Helvetica, sans-serif;
+    }
+  }
+}
+
+.loading {
+  padding-top: 5rem;
+  text-align: center;
+
+  svg {
+    margin-bottom: 1rem;
+    font-size: 3rem;
+  }
+}
+
+.search-prompt {
+  padding-top: 5rem;
+  text-align: center;
+
+  div.icon-container {
+    position: relative;
+    margin-bottom: 2rem;
+    svg {
+      position: relative;
+      font-size: 4rem;
+    }
+
+    .primary {
+      animation: moveplz 5s ease-in-out infinite;
+    }
+
+    .secondary {
+      position: absolute;
+      top: 0;
+      left: 50%;
+      animation: moveplz 5s ease-in-out infinite;
+      animation-direction: reverse;
+      animation-delay: 0.5s;
+      opacity: 0.2;
+      font-size: 3rem;
+    }
+  }
+
+  p {
+    font-size: 1.2rem;
+  }
+
+  @keyframes moveplz {
+    0%,
+    100% {
+      transform: translateX(0) translateY(0) rotateZ(0deg);
+    }
+
+    25% {
+      transform: translateX(-10px) translateY(10px) rotateZ(-30deg);
+    }
+
+    50% {
+      transform: translateX(10px) translateY(-10px) rotateZ(180deg);
+    }
+
+    75% {
+      transform: translateX(10px) translateY(10px) rotateZ(140deg);
+    }
+  }
+}
+
+.no-results {
+  display: flex;
+  width: 70%;
+  margin: 0 auto;
+  align-items: center;
+  margin-top: 5rem;
+
+  .text {
+    .title {
+      font-size: 1.2rem;
+      font-weight: bold;
+      margin-bottom: 0.5rem;
+    }
+  }
+
+  .fancy {
+    margin-right: 2rem;
+    text-align: center;
+
+    .missing-box {
+      width: 100px;
+      .inverse {
+        fill: black;
+        animation: box-one 8s ease-in-out infinite;
+      }
+
+      .prim-light {
+        fill: #f200f3;
+        animation: box-two 8s ease-in-out infinite;
+        animation-delay: 1s;
+      }
+
+      .prim-dark {
+        fill: #980098;
+        animation: box-three 8s ease-in-out infinite;
+        animation-delay: 1s;
+      }
+
+      @keyframes box-one {
+        0%,
+        100% {
+          fill: black;
+        }
+        50% {
+          fill: #f200f3;
+        }
+      }
+
+      @keyframes box-two {
+        0%,
+        100% {
+          fill: #f200f3;
+        }
+        50% {
+          fill: #161616;
+        }
+      }
+
+      @keyframes box-three {
+        0%,
+        100% {
+          fill: #980098;
+        }
+        50% {
+          fill: black;
+        }
+      }
     }
   }
 }
