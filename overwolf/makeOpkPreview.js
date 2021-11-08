a09743028c730c59f39fac1bf50891c8a54307e8
Michael Hillcox <michaelhillcox@gmail.com>
2021-11-08 16:52:18 +0000

    fix: shortcut issues

--- overwolf/makeOpkPreview.js
+++ overwolf/makeOpkPreview.js
@@ -3,6 +3,7 @@ const childProcess = require('child_process');
 let manifestFile = fs.readFileSync('manifest.json', 'utf-8');
 let manifestData = JSON.parse(manifestFile);
 let version = manifestData.meta.version;
+
 function makeOPK() {
   return childProcess.execSync(
     'zip -9 -r "' +
@@ -11,7 +12,8 @@ function makeOPK() {
       version +
       '.opk" dist/ jdk-11.0.8+10-jre/ FTBOverwolfShim.dll ' +
       manifestData.meta.icon +
-      ' *.jar assets/launchericon.ico manifest.json version.json -x windows/*',
+      ' *.jar launchericon.ico manifest.json version.json -x windows/*',
   );
 }
+
 makeOPK();
