ae7f34d67063d1e9160c15834763d7176c0bf7bc
covers1624 <laughlan.cov@internode.on.net>
2022-11-23 17:39:20 +1030

    Add json proxy as mirror.

--- subprocess/src/main/java/net/creeperhost/creeperlauncher/Constants.java
+++ subprocess/src/main/java/net/creeperhost/creeperlauncher/Constants.java
@@ -40,6 +40,7 @@ public class Constants {
     public static final String CREEPERHOST_MODPACK = CreeperLauncher.isDevMode ? "https://modpack-api.ch.tools" : "https://api.modpacks.ch";
     public static final String CREEPERHOST_MODPACK_SEARCH2 = CREEPERHOST_MODPACK + "/public/modpack/";
     public static final String MOD_API = CREEPERHOST_MODPACK + "/public/mod/";
+    public static final String JSON_PROXY = "https://api.modpacks.ch/public/meta/proxy/";
 
     public static final String CH_MAVEN = "https://maven.creeperhost.net/";
     public static final String MC_JSONS = "https://apps.modpacks.ch/versions/minecraftjsons/";
