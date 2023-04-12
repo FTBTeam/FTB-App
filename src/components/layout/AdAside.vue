cfe6590be43002284428b2b450a8a945ce6ac22c
Michael Hillcox <michaelhillcox@gmail.com>
2023-04-12 13:24:09 +0100

    fix: use correct ads code...

--- src/components/layout/AdAside.vue
+++ src/components/layout/AdAside.vue
@@ -1,51 +1,48 @@
 <template>
   <div class="ad-aside" :class="{ 'is-dev': isDev }">
-    
-<!--    <div class="ftb-ad-frame" v-if="!isDevEnv">-->
-<!--      <ins :data-revive-zoneid="adZone" data-revive-target="_blank" data-revive-id="3c373f2ff71422c476e109f9079cb399"></ins>-->
-<!--    </div>-->
+    <!--    <div class="ftb-ad-frame" v-if="!isDevEnv">-->
+    <!--      <ins :data-revive-zoneid="adZone" data-revive-target="_blank" data-revive-id="3c373f2ff71422c476e109f9079cb399"></ins>-->
+    <!--    </div>-->
 
-
-    <div class="ad-head-test mt-2 mb-4" v-if="!isElectron && noAdShowing">
-      <p class="font-bold">Ads support FTB & Mod Authors ❤️</p>
-    </div>
-    
-    <div class="ad-space" :key="adSpaceKey">
-<!--      <div class="ad-box mb-4" :class="{ 'overwolf-smaller': !isElectron }" v-if="!isElectron">-->
-<!--        <div class="flex flex-col w-full mt-auto mb-auto" v-show="advertsEnabled">-->
-<!--          <div-->
-<!--            v-if="!isElectron"-->
-<!--            id="ow-ad-second"-->
-<!--            ref="adRefSecond"-->
-<!--            style="max-width: 300px; max-height: 250px; display: flex; margin: 0 auto"-->
-<!--          />-->
-<!--          <div class='place-holder small' v-if="!isElectron && showPlaceholder['ow-ad-second']"><img class="logo" src="@/assets/images/ftb-logo.svg" alt="FTB Logo" />Thank you for using the FTB App</div>-->
-<!--        </div>-->
+    <div class="ad-container ads" v-if="!isElectron" key="adside-ad-type">
+<!--      <div class="ad-holder small mb-4">-->
+<!--        <div-->
+<!--          v-if="!isElectron"-->
+<!--          v-show="advertsEnabled ?? true"-->
+<!--          id="ow-ad-second"-->
+<!--          ref="adRefSecond"-->
+<!--          style="max-width: 300px; max-height: 250px;"-->
+<!--        />-->
+<!--        <div class='place-holder small' v-if="!isElectron && showAdTwoPlaceholder"><img class="logo" src="@/assets/images/ftb-logo.svg" alt="FTB Logo" />Thank you for using the FTB App</div>-->
 <!--      </div>-->
-      <div class="ad-box" :class="{ overwolf: !isElectron }">
-        <div class="flex flex-col w-full mt-auto mb-auto" v-show="advertsEnabled">
-          <div
-            v-if="!isElectron"
-            id="ow-ad"
-            ref="adRef"
-            style="max-width: 400px; max-height: 300px; display: flex; margin: 0 auto"
-          />
-          <div class='place-holder' v-if="!isElectron && showPlaceholder['ow-ad']"><span>❤️</span>These ads support FTB & CurseForge Mod Authors</div>
-          <video
-            @click="platform.get.utils.openUrl('https://go.ftb.team/creeperhost')"
-            class="cursor-pointer"
-            width="400"
-            height="300"
-            autoplay
-            muted
-            loop
-            style="margin: 0 auto"
-            v-if="isElectron && !isDevEnv && advertsEnabled"
-          >
-            <source src="https://dist.modpacks.ch/windows_desktop_src_assets_CH_AD.mp4" type="video/mp4" />
-          </video>
-        </div>
+
+      <div class="ad-holder">
+        <div
+          v-if="!isElectron"
+          v-show="advertsEnabled ?? true"
+          id="ow-ad"
+          ref="adRef"
+          style="max-width: 400px; max-height: 300px;"
+        />
+
+        <div class='place-holder' v-if="!isElectron && showAdOnePlaceholder"><span>❤️</span>These ads support FTB & CurseForge Mod Authors</div>
       </div>
+    </div>
+
+    <div class="ad-container ads-electron electron" v-else key="adside-ad-type">
+      <video
+        @click="platform.get.utils.openUrl('https://go.ftb.team/creeperhost')"
+        class="cursor-pointer"
+        width="300"
+        height="250"
+        autoplay
+        muted
+        loop
+        style="margin: 0 auto"
+        v-if="isElectron && !isDevEnv && advertsEnabled"
+      >
+        <source src="https://dist.modpacks.ch/windows_desktop_src_assets_CH_AD.mp4" type="video/mp4" />
+      </video>
     </div>
   </div>
 </template>
@@ -54,7 +51,7 @@
 import Vue from 'vue';
 import platform from '@/utils/interface/electron-overwolf';
 import Component from 'vue-class-component';
-import { Action, State } from 'vuex-class';
+import { State } from 'vuex-class';
 import { SettingsState } from '@/modules/settings/types';
 import { AuthState } from '@/modules/auth/types';
 import { Prop } from 'vue-property-decorator';
@@ -64,25 +61,24 @@ import { getLogger } from '@/utils';
 export default class AdAside extends Vue {
   @State('settings') public settings!: SettingsState;
   @State('auth') public auth!: AuthState;
-  @Action('reportAdvert') public reportAd!: any;
   @Prop({ default: false }) public isDev!: boolean;
 
   private logger = getLogger('ad-aside-vue');
 
   ads: Record<string, any> = {};
   platform = platform;
-  showPlaceholder: Record<string, boolean> = {};
+  showAdOnePlaceholder = true;
+  showAdTwoPlaceholder = true;
 
   random = AdAside.mkRandom();
-  adSpaceKey = 'ad-space-key'
-  
+
   get isElectron() {
     return platform.isElectron();
   }
 
   async mounted() {
     this.logger.info('Loaded ad sidebar widget');
-    
+
     // I've lost brain cells from this. Please just fucking work 
     // - Covers
     // if (!this.isDevEnv) {
@@ -91,99 +87,73 @@ export default class AdAside extends Vue {
     //   helpMe.async = true;
     //   document.head.append(helpMe)
     // }
-    
+
     // Kinda dirty hack for this file
     if (this.isElectron) {
       return;
     }
 
     setTimeout(() => {
-      this.loadAds("ow-ad", {size: { width: 400, height: 300 }});
-      // this.loadAds("ow-ad-second", {size: {width: 300, height: 250}});
+      this.loadAds("ad-1", (value) => this.showAdOnePlaceholder = value, this.$refs.adRef, {size: { width: 400, height: 300 }});
+      // this.loadAds("ad-2", (value) => this.showAdTwoPlaceholder = value, this.$refs.adRefSecond, {size: {width: 300, height: 250}});
     }, 1500);
   }
 
-  async loadAds(id: string, options?: any) {    
-    this.showPlaceholder[id] = true;
-    this.adSpaceKey = 'ad-space-key' + Math.random();
-    
-    // Wait for the view to update so getting the elm doesn't fail
-    await new Promise((res) => {
-      this.$nextTick(() => res(null))
-    });
-    
+  async loadAds(id: string, emitPlaceholderUpdate: (state: boolean) => void, elm: any, options?: any) {
+    emitPlaceholderUpdate(true);
+
     if (this.isDevEnv) {
       return;
     }
-    
-    this.logger.info('Loading advert system for ' + id);
-    //@ts-ignore
+
+    this.logger.info(`[AD: ${id}] Loading advert system for ${id}`);
+
     if (typeof OwAd === 'undefined' || !OwAd) {
-      this.showPlaceholder[id] = true;
-      this.adSpaceKey = 'ad-space-key' + Math.random();
-      this.logger.info('No advert object available');
-    } else {
-      //@ts-ignore
-      if (window.ads && window.ads[id]) {
-        this.logger.info('Is window advert');
-        //@ts-ignore
-        this.ads[id] = window.ads[id];
-        this.ads[id].refreshAd();
-      } else {
-        this.logger.info('Created advert');
-        //@ts-ignore
-        this.ads[id] = new OwAd(document.getElementById(id), options);
-        this.logger.info(this.ads[id], document.getElementById(id));
-        this.logger.info("Loaded ads");
-
-        //@ts-ignore
-        if (!window.ads) {
-          //@ts-ignore
-          
-          window.ads = {};
-        }
-        
-        //@ts-ignore
-        window.ads[id] = this.ads[id];
-      }
-
-      this.ads[id].addEventListener('error', (error: any) => {
-        this.showPlaceholder[id] = true;
-        this.adSpaceKey = 'ad-space-key' + Math.random();
-        console.log(error)
-        this.logger.info('Failed to load ad');
-        this.logger.info(error);
-      });
-      this.ads[id].addEventListener('player_loaded', () => {
-        this.showPlaceholder[id] = false;
-        this.adSpaceKey = 'ad-space-key' + Math.random();
-        this.logger.info('player loaded for overwolf');
-      });
-      this.ads[id].addEventListener('display_ad_loaded', () => {
-        this.showPlaceholder[id] = false;
-        this.adSpaceKey = 'ad-space-key' + Math.random();
-        this.logger.info('display_ad_loaded overwolf');
-        fetch(`${process.env.VUE_APP_MODPACK_API}/public/analytics/ads/static`);
-      });
-      this.ads[id].addEventListener('play', () => {
-        this.showPlaceholder[id] = false;
-        this.adSpaceKey = 'ad-space-key' + Math.random();
-        this.logger.info('play overwolf');
-      });
-      this.ads[id].addEventListener('complete', () => {
-        this.logger.info('complete ads for overwolf');
-      });
-      this.ads[id].addEventListener('impression', () => {
-        fetch(`${process.env.VUE_APP_MODPACK_API}/public/analytics/ads/video`);
-      });
+      emitPlaceholderUpdate(true);
+      this.logger.info(`[AD: ${id}] No advert object available`);
+      return;
     }
+
+    this.logger.info(`[AD: ${id}] Created advert object`);
+
+    this.ads[id] = new OwAd(elm, options);
+    
+    const win = window as any;
+    if (!win.ads) {
+      win.ads = {};
+    } 
+    
+    win.ads[id] = this.ads[id];
+    this.ads[id].addEventListener('error', (error: any) => {
+      emitPlaceholderUpdate(true);
+      this.logger.info(`[AD: ${id}] Failed to load ad`);
+      console.log(`[AD: ${id}]`, error)
+    });
+    this.ads[id].addEventListener('player_loaded', () => {
+      this.logger.info(`[AD: ${id}] Player loaded`);
+    });
+    this.ads[id].addEventListener('display_ad_loaded', () => {
+      emitPlaceholderUpdate(false);
+      this.logger.info(`[AD: ${id}] Display ad loaded and ready`);
+      fetch(`${process.env.VUE_APP_MODPACK_API}/public/analytics/ads/static`);
+    });
+    this.ads[id].addEventListener('play', () => {
+      emitPlaceholderUpdate(false);
+      this.logger.info(`[AD: ${id}] Ad ready and loaded`);
+    });
+    this.ads[id].addEventListener('complete', () => {
+      this.logger.info(`[AD: ${id}] Video ad finished playing`);
+    });
+    this.ads[id].addEventListener('impression', () => {
+      fetch(`${process.env.VUE_APP_MODPACK_API}/public/analytics/ads/video`);
+    });
   }
-  
+
   get advertsEnabled(): boolean {
     if (!this.auth?.token?.activePlan) {
       return true;
     }
-    
+
     // If this fails, show the ads
     return (this.settings?.settings?.showAdverts === true || this.settings?.settings?.showAdverts === 'true') ?? true;
   }
@@ -191,19 +161,13 @@ export default class AdAside extends Vue {
   static mkRandom() {
     return Math.round(Math.random() * 999999999999);
   }
-  
+
   get adZone() {
     return this.platform.isElectron() ? "4" : "1"
   }
-  
+
   get isDevEnv() {
     return process.env.NODE_ENV !== 'production';
-  }
-  
-  get noAdShowing() {
-    // Change to this one if we start showing two ads
-    // return !(this.showPlaceholder['ow-ad-second'] && this.showPlaceholder['ow-ad']);
-    return !this.showPlaceholder['ow-ad'];
   }
 }
 </script>
@@ -213,96 +177,72 @@ export default class AdAside extends Vue {
   padding: 1rem;
   background: rgb(17 17 17);
 
+  min-width: calc(300px + 2.5rem); // Why? Fuck knows, Thanks css
+
   display: flex;
   flex-direction: column;
   align-items: center;
-
   justify-content: flex-end;
-  transition: background-color 0.3s ease-in-out;
-  position: relative;
-  //z-index: 1; // Breaks modals atm
+
   border-left: 2px solid rgba(black, 0.1);
 
-  //&::after {
-  //  content: '';
-  //  width: 100%;
-  //  height: 100%;
-  //  position: absolute;
-  //  background: url('../../assets/backgrounds/adaside.png') center top no-repeat;
-  //  background-size: auto 1080px;
-  //  top: 0;
-  //  left: 0;
-  //  z-index: -1;
-  //  opacity: 0.5;
-  //}
+  .ads {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    position: relative;
+    z-index: 1;
 
-  .ftb-ad-frame {
-    width: 100%;
-    display: flex;
-    justify-content: flex-start;
-    flex-direction: column;
-    align-items: center;
-    flex: 1;
-  }
-  
-  .place-holder {
-    width: 400px;
-    height: 300px;
-    background: black;
-    border-radius: 5px;
-    
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    
-    flex-direction: column;
-    padding: 3rem;
-    text-align: center;
-    font-weight: bold;
-    
-    span {
-      display: block;
-      margin-bottom: 1rem;
-    }
-    
-    .logo {
-      max-width: 60px;
-      margin-bottom: 1rem;
-    }
-    
-    &.small {
-      width: 300px;
-      height: 250px;
-      padding: 2rem;
-    }
-  }
-  
-  .ad-space {
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    
-    .ad-head-test {
-      max-width: 300px;
-    }
-
-    .ad-box {
-      //background: black;
-      width: 300px;
-      height: 250px;
-      overflow: hidden;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      
-      &.overwolf {
-        width: 400px;
-        height: 300px;
+    &.ad-container {
+      width: 400px;
+      &.electron {
+        width: 300px;
       }
-      
-      &.overwolf-smaller {
+    }
+
+    .ad-holder {
+      position: relative;
+      width: 400px;
+      height: 300px;
+
+      &.small {
         width: 300px;
         height: 250px;
+      }
+
+      .place-holder {
+        position: absolute;
+        width: 400px;
+        height: 300px;
+        background: black;
+        border-radius: 5px;
+        top: 0;
+        left: 0;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        flex-direction: column;
+        padding: 3rem;
+        text-align: center;
+        font-weight: bold;
+        z-index: -1;
+
+        span {
+          display: block;
+          margin-bottom: 1rem;
+        }
+
+        .logo {
+          max-width: 60px;
+          margin-bottom: 1rem;
+        }
+
+        &.small {
+          height: 250px;
+          left: -50px;
+        }
       }
     }
   }
