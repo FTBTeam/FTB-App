40b60fb89095472cfbfc435659a56a4a06786c7b
Michael Hillcox <michaelhillcox@gmail.com>
2023-07-21 21:16:17 +0100

    chore: removed dead code

--- src/components/templates/modpack/ModpackVersions.vue
+++ src/components/templates/modpack/ModpackVersions.vue
@@ -5,45 +5,8 @@
         :badge-end="true"
         :options="packVersions"
         :inheritedSelection="packVersions[0]"
-        @selected="version => loadChanges(version)"
+        @selected="(version) => loadChanges(version)"
       />
-
-<!--      <div class="items">-->
-<!--        <div-->
-<!--          class="item"-->
-<!--          :class="{ active: version.id === activeLog }"-->
-<!--          v-for="(version, index) in versions"-->
-<!--          :key="index"-->
-<!--        >-->
-<!--          <div-->
-<!--            class="main"-->
-<!--            :class="{ disabled: version.type.toLowerCase() === 'archived' }"-->
-<!--            @click="() => loadChanges(version.id)"-->
-<!--          >-->
-<!--            <header class="flex justify-between flex-wrap">-->
-<!--              <div class="version">{{ version.name }}</div>-->
-<!--              <div class="updated">{{ version.updated | dayjsFromNow }}</div>-->
-<!--            </header>-->
-<!--            <div class="type-data">-->
-<!--              <div-->
-<!--                class="type"-->
-<!--                :style="{-->
-<!--                  backgroundColor:-->
-<!--                    version.type.toLowerCase() === 'release'-->
-<!--                      ? '#27AE60'-->
-<!--                      : version.type.toLowerCase() === 'beta'-->
-<!--                      ? '#00a8FF'-->
-<!--                      : 'black',-->
-<!--                }"-->
-<!--              >-->
-<!--                {{ version.type }}-->
-<!--              </div>-->
-<!--              <div class="current" v-if="version.id === current">Current</div>-->
-<!--              <div class="latest" v-if="index === 0">Latest</div>-->
-<!--            </div>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </div>-->
     </div>
     <div class="main flex pb-8 flex-col" :key="activeLog">
       <div class="heading flex items-center mb-4" v-if="currentVersion">
@@ -205,7 +168,6 @@ export default class ModpackVersions extends Vue {
 
 <style scoped lang="scss">
 .pack-versions {
-  //display: flex;
   height: 100%;
   position: relative;
   font-size: 0.875rem;
@@ -215,86 +177,12 @@ export default class ModpackVersions extends Vue {
   .aside,
   .main {
     > p {
-      //margin-bottom: 1rem;
       font-size: 1.25rem;
     }
   }
 
-  .aside {
-    //margin-right: 2rem;
-    //overflow-y: auto;
-    //height: 100%;
-
-  //  .items {
-  //    //width: 280px;
-  //
-  //    .item {
-  //      display: flex;
-  //      align-items: center;
-  //      justify-content: space-between;
-  //
-  //      margin-bottom: 1rem;
-  //      padding: 0.8rem 1rem;
-  //      border-radius: 5px;
-  //      cursor: pointer;
-  //      transition: background-color 0.25s ease-in-out;
-  //
-  //      &.active,
-  //      &:hover {
-  //        background-color: var(--color-background);
-  //      }
-  //
-  //      .main.disabled {
-  //        opacity: 0.3;
-  //      }
-  //
-  //      .type-data {
-  //        display: flex;
-  //
-  //        %type {
-  //          padding: 0.1rem 0.3rem;
-  //          border-radius: 3px;
-  //          background-color: #595959;
-  //          font-size: 0.875rem;
-  //          text-transform: capitalize;
-  //          display: inline-block;
-  //        }
-  //
-  //        .type,
-  //        .latest,
-  //        .current {
-  //          @extend %type;
-  //          margin-right: 0.5rem;
-  //        }
-  //
-  //        .latest {
-  //          background-color: var(--color-info-button);
-  //        }
-  //
-  //        .current {
-  //          background-color: var(--color-warning-button);
-  //        }
-  //      }
-  //
-  //      .version {
-  //        font-weight: bold;
-  //        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif,
-  //          'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
-  //        margin-bottom: 0.4rem;
-  //      }
-  //
-  //      .updated {
-  //        font-size: 0.875rem;
-  //        opacity: 0.8;
-  //      }
-  //    }
-  //  }
-  }
-
   .main {
     flex: 1;
-    //overflow-x: auto;
-    //height: 100%;
   }
 
   .closer {
