807298c0d583ae269950e96faac7deedc3716c67
covers1624 <covers1624@gmail.com>
2023-09-01 19:56:07 +0930

    give syncConflict a type.

--- subprocess/src/main/java/net/creeperhost/creeperlauncher/api/data/instances/InstanceCloudSyncConflictData.java
+++ subprocess/src/main/java/net/creeperhost/creeperlauncher/api/data/instances/InstanceCloudSyncConflictData.java
@@ -14,6 +14,7 @@ public class InstanceCloudSyncConflictData extends BaseData {
     public final String message;
 
     public InstanceCloudSyncConflictData(UUID uuid, String code, String message) {
+        type = "syncConflict";
         this.uuid = uuid;
         this.code = code;
         this.message = message;
