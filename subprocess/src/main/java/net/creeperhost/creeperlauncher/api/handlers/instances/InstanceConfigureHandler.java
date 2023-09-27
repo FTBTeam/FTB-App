aaa6716c7912b92c06bc8eaceb4d41aa7b9d55a0
covers1624 <covers1624@gmail.com>
2023-09-27 15:48:15 +0930

    Cleanup..

--- subprocess/src/main/java/net/creeperhost/creeperlauncher/api/handlers/instances/InstanceConfigureHandler.java
+++ subprocess/src/main/java/net/creeperhost/creeperlauncher/api/handlers/instances/InstanceConfigureHandler.java
@@ -4,50 +4,50 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.covers1624.jdkutils.JavaLocator;
 import net.covers1624.quack.gson.JsonUtils;
-import net.creeperhost.creeperlauncher.Settings;
 import net.creeperhost.creeperlauncher.Instances;
+import net.creeperhost.creeperlauncher.Settings;
 import net.creeperhost.creeperlauncher.api.data.instances.InstanceConfigureData;
 import net.creeperhost.creeperlauncher.api.handlers.IMessageHandler;
 import net.creeperhost.creeperlauncher.pack.Instance;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Map;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class InstanceConfigureHandler implements IMessageHandler<InstanceConfigureData> {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
     @Override
     public void handle(InstanceConfigureData data) {
-        try
-        {
+        try {
             Instance instance = Instances.getInstance(data.uuid);
             if (instance == null) {
-                // TODO, This message needs to be improved. We should tell the frontend _why_
                 Settings.webSocketAPI.sendMessage(new InstanceConfigureData.Reply(data, "error", "Unable to save settings as the instance couldn't be found..."));
                 return;
             }
 
             JsonObject updateJson = JsonUtils.parseRaw(data.instanceJson).getAsJsonObject();
-            
+
             instance.props.name = getOrDefault(updateJson, "name", JsonElement::getAsString, instance.props.name);
             instance.props.jvmArgs = getOrDefault(updateJson, "jvmArgs", JsonElement::getAsString, instance.props.jvmArgs);
-            
+
             Path jreRealPath = instance.props.jrePath;
             if (updateJson.has("jrePath")) {
                 String jrePath = updateJson.get("jrePath").getAsString();
                 jreRealPath = Paths.get(jrePath);
             }
-            
+
             instance.props.jrePath = jreRealPath;
             instance.props.memory = getOrDefault(updateJson, "memory", JsonElement::getAsInt, instance.props.memory);
             instance.props.width = getOrDefault(updateJson, "width", JsonElement::getAsInt, instance.props.width);
             instance.props.height = getOrDefault(updateJson, "height", JsonElement::getAsInt, instance.props.height);
             instance.props.cloudSaves = getOrDefault(updateJson, "cloudSaves", JsonElement::getAsBoolean, instance.props.cloudSaves);
             instance.props.embeddedJre = jreRealPath == null;
-            
+
             if (jreRealPath != null) {
                 if (JavaLocator.parseInstall(jreRealPath) == null) {
                     Settings.webSocketAPI.sendMessage(new InstanceConfigureData.Reply(data, "error", "No java install found... Make sure you're selecting the 'java' file in '/bin'."));
@@ -55,11 +55,11 @@ public class InstanceConfigureHandler implements IMessageHandler<InstanceConfigu
                 }
                 instance.props.jrePath = jreRealPath;
             }
-            
+
             instance.saveJson();
             Settings.webSocketAPI.sendMessage(new InstanceConfigureData.Reply(data, "success", "", new InstalledInstancesHandler.SugaredInstanceJson(instance)));
-        } catch (Exception err) {
-            err.printStackTrace();
+        } catch (IOException ex) {
+            LOGGER.error("Failed to update instance settings.", ex);
             Settings.webSocketAPI.sendMessage(new InstanceConfigureData.Reply(data, "error", "Fatal error on settings saving..."));
         }
     }
