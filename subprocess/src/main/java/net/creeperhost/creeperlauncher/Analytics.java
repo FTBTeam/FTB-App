2b554dd32a56fbc6b18c92ed3f55df02ea2bb655
Michael Hillcox <michaelhillcox@gmail.com>
2024-02-29 13:08:19 +0000

    fix: double whoops

--- subprocess/src/main/java/net/creeperhost/creeperlauncher/Analytics.java
+++ subprocess/src/main/java/net/creeperhost/creeperlauncher/Analytics.java
@@ -1,5 +1,6 @@
 package net.creeperhost.creeperlauncher;
 
+import net.creeperhost.creeperlauncher.util.ModpacksChUtils;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
@@ -7,22 +8,21 @@ import java.util.concurrent.CompletableFuture;
 
 public class Analytics {
     public static void sendInstallRequest(long packID, long packVersion, byte packType) {
-//        Request.Builder analytics = Constants.createModpacksEndpoint(false, packType, "/" + packID + "/" + packVersion + "/install");
-//        lazyGetRequest(analytics);
+        lazyGetRequest(ModpacksChUtils.getModpacksEndpoint(false, packType) + "/" + packID + "/" + packVersion + "/install");
     }
 
     public static void sendPlayRequest(long packID, long packVersion, byte packType) {
-//        lazyGetRequest(Constants.createModpacksEndpoint(false, packType, "/" + packID + "/" + packVersion + "/play"));
+        lazyGetRequest(ModpacksChUtils.getModpacksEndpoint(false, packType) + "/" + packID + "/" + packVersion + "/play");
     }
 
     /**
      * We do not care if this fails, so we gobble the exception.
      */
-    private static void lazyGetRequest(Request.Builder builder) {
+    private static void lazyGetRequest(String endpoint) {
         CompletableFuture.runAsync(() -> {
             try {
                 OkHttpClient okHttpClient = Constants.httpClient();
-                okHttpClient.newCall(builder.build()).execute();
+                okHttpClient.newCall(new Request.Builder().url(endpoint).build()).execute();
             } catch (Exception e) {
                 // We don't care if this fails.
             }
