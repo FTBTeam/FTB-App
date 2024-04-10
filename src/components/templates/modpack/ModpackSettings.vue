934817443fecd59bc4b14ae549c6af8a52d290a9
Michael Hillcox <michaelhillcox@gmail.com>
2024-04-10 11:08:32 +0100

    fix: merge issue from #1097

--- src/components/templates/modpack/ModpackSettings.vue
+++ src/components/templates/modpack/ModpackSettings.vue
@@ -263,7 +263,7 @@ import UiButton from '@/components/core/ui/UiButton.vue';
 import {instanceInstallController} from '@/core/controllers/InstanceInstallController';
 import {resolveModloader, resolveModLoaderVersion, typeIdToProvider} from '@/utils/helpers/packHelpers';
 import CategorySelector from '@/components/core/modpack/create/CategorySelector.vue';
-import {computeAspectRatio, prettyByteFormat, megabyteSize} from '@/utils';
+import {computeAspectRatio, megabyteSize, prettyByteFormat} from '@/utils';
 import UiToggle from '@/components/core/ui/UiToggle.vue';
 import ModloaderSelect from '@/components/core/modpack/components/ModloaderSelect.vue';
 import {ModLoaderWithPackId} from '@/core/@types/modpacks/modloaders';
@@ -302,6 +302,8 @@ export default class ModpackSettings extends Vue {
   instanceSettings: SaveJson = {} as any;
   previousSettings: SaveJson = {} as any;
 
+  megabyteSize = megabyteSize
+  
   showDuplicate = false;
   shareConfirm = false;
   jreSelection = '';
