e4c55cee0eb2d1437399025ecb90f5b7e37fd4ea
Gareth Williams <gaz492@gmail.com>
2026-07-29 08:21:08 +0100

    feat: add hover background to instance mod list

--- src/components/groups/instance/ModpackMods.vue
+++ src/components/groups/instance/ModpackMods.vue
@@ -258,7 +258,7 @@ const installedMods = computed<[number, number][]>(() => {
     <div class="mods">      
       <p class="text-center italic opacity-80" v-if="!packMods.length">{{ instance.name }} does not have any mods installed</p>
       <recycle-scroller v-else class="complex-mod mod" :items="packMods" :item-size="54" key-field="index" v-slot="{ item }">
-        <div class="flex gap-6 items-center mb-4">
+        <div class="p-2 flex gap-6 items-center mb-4 hover:bg-[#202020] rounded-lg">
           <img v-if="item.curse && item.curse.icon" :src="item.curse.icon" class="rounded" width="40" alt="">
           <div class="placeholder bg-black rounded mt-2" style="width: 40px; height: 40px" v-else-if="item.fileName !== ''"></div>
           
